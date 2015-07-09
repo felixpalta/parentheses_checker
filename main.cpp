4ede8f5c3b27aa427368bc4d1f621a5d172b7442
Felix Palta <fpalta90@gmail.com>
2015-07-10 00:04:36 +0300

    Implement class ParenthesesChecker.

--- main.cpp
+++ main.cpp
@@ -1,39 +1,107 @@
 #include <iostream>
 #include <stack>
+#include <vector>
+#include <algorithm>
+#include <utility>
 
 using namespace std;
+
+class ParenthesesChecker
+{
+public:
+
+    void add_brace_pair(char opening, char closing)
+    {
+        if (is_opening(opening) || is_closing(closing))
+            return; // These symbols are already present.
+        used_pairs.push_back(BracePair{opening, closing});
+    }
+
+    bool check_expression(const string& s)
+    {
+        char c;
+        unsigned i;
+        for (i = 0; i < s.length(); ++i)
+        {
+            c = s[i];
+            if (is_opening(c))
+                brace_stack.push(c);
+            else if (is_closing(c))
+            {
+                if (brace_stack.empty())
+                    break;
+                if (!braces_match(brace_stack.top(), c))
+                    break;
+                brace_stack.pop();
+            }
+        }
+
+        if (brace_stack.empty() && i == s.length())
+            return true;
+        return false;
+    }
+private:
+
+    using BracePair = pair<char, char>;
+
+    class CheckOpening
+    {
+        char c_;
+    public:
+        CheckOpening(char c) : c_(c) {}
+        bool operator()(const BracePair& p)
+        {
+            return p.first == c_;
+        }
+    };
+
+    class CheckClosing
+    {
+        char c_;
+    public:
+        CheckClosing(char c) : c_(c) {}
+        bool operator()(const BracePair& p)
+        {
+            return p.second == c_;
+        }
+    };
+
+
+    bool is_opening(char c) const
+    {
+        auto iter = find_if(begin(used_pairs), end(used_pairs),CheckOpening{c});
+        return iter != end(used_pairs);
+    }
+    bool is_closing(char c) const
+    {
+        auto iter = find_if(begin(used_pairs), end(used_pairs), CheckClosing{c});
+        return iter != end(used_pairs);
+    }
+
+    bool braces_match(char opening, char closing) const
+    {
+        auto iter = find(begin(used_pairs), end(used_pairs), BracePair{opening, closing});
+        return iter != end(used_pairs);
+    }
+    stack<char> brace_stack;
+    vector<BracePair> used_pairs;
+};
+
+
 
 int main()
 {
     string s;
+
+    ParenthesesChecker pch{};
+    pch.add_brace_pair('(', ')');
+    pch.add_brace_pair('{', '}');
+
     cout << "Enter an expression with parentheses." << endl;
     getline(cin, s);
 
-    class Stub
-    {
-    };
-    stack<Stub> st;
 
-    unsigned i = 0;
-
-    for (auto& c : s)
-    {
-        if (c == '(')
-            st.push(Stub{});
-        else if (c == ')')
-        {
-            if (st.empty())
-                break;
-            else
-                st.pop();
-        }
-        ++i;
-    }
-
-    if (st.empty() && i == s.length())
-        cout << "Expression is valid" << endl;
-    else
-        cout << "Expression is invalid" << endl;
+    cout << boolalpha << pch.check_expression(s) << endl;
 
     return 0;
 }
